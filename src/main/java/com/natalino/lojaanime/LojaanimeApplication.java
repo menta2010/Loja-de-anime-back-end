eb798d97dee55a0a7b74c5cfbb1ac9335d883571
natalino <natalinoesteves2010@gmail.com>
2022-01-18 09:12:21 -0300

    criando projeto

---
+++ src/main/java/com/natalino/lojaanime/LojaanimeApplication.java
@@ -0,0 +1,13 @@
+package com.natalino.lojaanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaanimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaanimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natalino/lojaanime/LojaanimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natalino.lojaanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaanimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
